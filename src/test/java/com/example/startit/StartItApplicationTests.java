3840b5e3400c3f69dba2d789bf5e8dfdd749fd16
Bulat Musin <66408714+Fostonger@users.noreply.github.com>
2023-10-27 01:01:15 +0300

    initial commit

---
+++ src/test/java/com/example/startit/StartItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
